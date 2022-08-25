1a6bb6de3c1ee865e58d05e73ea40fbddf5fd720
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-08-25 17:21:35 -0400

    Add tasks file"

---
+++ src/tasks.js
@@ -0,0 +1,19 @@
+const todos = [
+  {
+    id: 1,
+    name: 'Temple visit',
+    priority: 'M',
+  },
+  {
+    id: 2,
+    name: 'Doctors appointement',
+    priority: 'L',
+  },
+  {
+    id: 3,
+    name: 'Temple visit',
+    priority: 'H',
+  },
+]
+
+export default todos
