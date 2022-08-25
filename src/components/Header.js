88cc948822ad16605c3b0710ebfe22800bcb6ad6
kaviramsv <kavitasvenkitachalam@gmail.com>
2022-08-25 16:55:50 -0400

    Add the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Header = ({ title }) => {
-
+  const headerStyles = {
+    color: '#ff6a95',
+    backgroundColor: 'rgba(0,0,0,.4)',
+    text: 'Task Tracker',
+  }
   return (
-    <h1>{title}</h1>
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{title}</h2>
+      </div>
+    </header>
   )
 }
 Header.propTypes = {
   title: PropTypes.string.isRequired
 }
 Header.defaultProps = {
-  title: "default",
+  // backgroundColor: '#ff6a95',
+  // color: 'rgba(0,0,0,.4)',
+  title: 'Task Tracker',
 }
 
 export default Header
